5cd171619c8db025a637c9f0eb5705174d4f6334
Haker0428 <513193084@qq.com>
2025-11-25 01:52:03 +0800

    add vector add

---
+++ day01/02_vectoradd/vector_add.cu
@@ -0,0 +1,110 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <math.h>
+
+#define CHECK(call)\
+{\
+  const cudaError_t error=call;\
+  if(error!=cudaSuccess)\
+  {\
+      printf("ERROR: %s:%d,",__FILE__,__LINE__);\
+      printf("code:%d,reason:%s\n",error,cudaGetErrorString(error));\
+      exit(1);\
+  }\
+}
+
+void initialData(float* ip,int size)
+{
+  time_t t;
+  srand((unsigned )time(&t));
+  for(int i=0;i<size;i++)
+  {
+    ip[i]=(float)(rand()&0xffff)/1000.0f;
+  }
+}
+
+void sumArrays(float *a, float *b, float *res, const int size)
+{
+    for (int i = 0; i < size; i += 4) {
+        res[i] = a[i] + b[i];
+        res[i+1]=a[i+1]+b[i+1];
+        res[i+2]=a[i+2]+b[i+2];
+        res[i+3]=a[i+3]+b[i+3];
+    }
+}
+
+void checkResult(float * hostRef,float * gpuRef,const int N)
+{
+  double epsilon=1.0E-8;
+  for(int i=0;i<N;i++)
+  {
+    if(fabs(hostRef[i]-gpuRef[i])>epsilon)
+    {
+      printf("Results don\'t match!\n");
+      printf("%f(hostRef[%d] )!= %f(gpuRef[%d])\n",hostRef[i],i,gpuRef[i],i);
+      return;
+    }
+  }
+  printf("Check result success!\n");
+}
+
+__global__ void sumArraysGpu(float *a, float * b, float *res, int n)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n)
+    {
+        res[i] = a[i] + b[i];
+    }
+}
+
+int main()
+{
+    int dev = 0;
+    cudaSetDevice(dev);
+
+    int nElem = 32;
+    printf("Vector size: %d\n", nElem);
+
+    int nByte = sizeof(float) * nElem;
+    float *a_h = (float*) malloc(nByte);
+    float *b_h = (float*) malloc(nByte);
+    float *res_h = (float*) malloc(nByte);
+    float *res_from_gpu_h = (float*) malloc(nByte);
+    memset(res_h, 0, nByte);
+    memset(res_from_gpu_h, 0, nByte);
+
+    float *a_d, *b_d, *res_d;
+    CHECK(cudaMalloc((float**)&a_d, nByte));
+    CHECK(cudaMalloc((float**)&b_d, nByte));
+    CHECK(cudaMalloc((float**)&res_d, nByte));
+
+    initialData(a_h, nElem);
+    initialData(b_h, nElem);
+
+    CHECK(cudaMemcpy(a_d, a_h, nByte, cudaMemcpyHostToDevice));
+    CHECK(cudaMemcpy(b_d, b_h, nByte, cudaMemcpyHostToDevice));
+
+    dim3 block(nElem);
+    dim3 grid(nElem / block.x);
+    sumArraysGpu<<<grid, block>>>(a_d, b_d, res_d, nElem);
+    CHECK(cudaDeviceSynchronize());  // 等待 GPU 完成执行
+    printf("Execution configuration<<<%d, %d>>>\n", block.x, grid.x);
+
+    CHECK(cudaMemcpy(res_from_gpu_h, res_d, nByte, cudaMemcpyDeviceToHost));
+
+    sumArrays(a_h, b_h, res_h, nElem);
+
+    checkResult(res_h, res_from_gpu_h, nElem);
+
+    cudaFree(a_d);
+    cudaFree(b_d);
+    cudaFree(res_d);
+    free(a_h);
+    free(b_h);
+    free(res_h);
+    free(res_from_gpu_h);
+    return 0;
+}
